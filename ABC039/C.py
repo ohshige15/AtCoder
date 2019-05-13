8969ff6b74b27f54bcd5e5afe75875ab4d174c47
ohshige <satoshi.o.xxx@gmail.com>
2019-05-13 21:54:50 +0900

    ABC039 A B C

---
+++ ABC039/C.py
@@ -0,0 +1,11 @@
+S = input()
+scales = {
+    "WBWBWWBWBWBWWBWBWWBW": "Do",
+    "WBWWBWBWBWWBWBWWBWBW": "Re",
+    "WWBWBWBWWBWBWWBWBWBW": "Mi",
+    "WBWBWBWWBWBWWBWBWBWW": "Fa",
+    "WBWBWWBWBWWBWBWBWWBW": "So",
+    "WBWWBWBWWBWBWBWWBWBW": "La",
+    "WWBWBWWBWBWBWWBWBWWB": "Si",
+}
+print(scales[S])
